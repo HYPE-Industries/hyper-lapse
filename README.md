99d12b13e2f96a25a55d5a98a1a1af0f426cf3fa
Evan Sellers <sellers.evan@hype-industries.com>
2020-05-08 16:14:52 -0400

    motion option

--- README.md
+++ README.md
@@ -15,7 +15,12 @@ This is the time between photos in seconds. This can either be set as a whole in
 **Camera (optional)**<br>
 The camera address can either be represented by an integer which represents local web cameras (starting at `0`) or an IP Camera address. IP cameras can be accessed either from the HTTP/HTTPS or RTSP protocols (We use OpenCV2 for video capture). For example to access a JPG stream from an IP Camera the address might be `http://192.169.1.19:8080/video`. This option is not required and the default option is `0` which should be your web camera.
 
+**Motion (optional)**<br>
+The motion flag will only save the image if there is a difference between the last frame captured. This means if you have a delay of 5 seconds and there is not difference between frame 1 and frame 2, 5 seconds later frame 2 will not be saved. You can also set the sensitivity using `-s` or `--sensitivity`. The default is `0.02`, and we don't recommend going above `0.05`. The sensitivity must be below `1`.
+
 #### Examples
 ```
 python index.py --output demo --delay 5
+python index.py --output demo --delay 5 --motion
+python index.py --output demo --delay 5 --motion --sensitivity 0.3
 ```
--- index.py
+++ index.py
@@ -11,14 +11,16 @@ import time;
 from os import system
 
 
-
 parser = argparse.ArgumentParser( description="Hyper Timelapse for Prism" );
 parser.add_argument( "--output","-o",required=True, help="set the output directory" );
 parser.add_argument( "--delay", "-d",required=True,type=float, help="seconds between picture" );
 parser.add_argument( "--camera", "-c",default="0", help="camera IP address or web camera number (openCV)" );
+parser.add_argument( "--motion", action="store_true", help="[Flag] Only Capture image if motion between delay frames" );
+parser.add_argument( "--sensitivity", "-s",default="0.03", type=float, help="How sentive the motion detector is." );
 args = parser.parse_args()
 system( "title "+"Hype Timelapse Program" );
 
+print(args.sensitivity)
 
 # check to make sure directory exists
 if not os.path.isdir( os.path.join( os.getcwd(), args.output ) ):
@@ -52,20 +54,41 @@ except:
 print( "Press [Esc] to exit program" );
 wait = time.time();
 screen_shot = 0;
+_, last_frame = cam.read();
+size = int( cam.get( 3 ) * cam.get( 4 ) );
 path = os.path.join( os.getcwd(), args.output );
 while True:
     _, frame = cam.read()
     cv2.imshow( "Hyper Timelapse for Prism", cv2.flip( frame, 1 ) );
     if wait < time.time():
-        try:
-            print( os.path.join( path, "img_" + str( int( time.time() ) ) + "_n" + str( screen_shot ) + ".jpg" ) )
-            cv2.imwrite( os.path.join( path, "img_" + str( int( time.time() ) ) + "_n" + str( screen_shot ) + ".jpg" ), frame );
-        except:
-            print( "[ ERRROR ] Writing File" );
+        capture = True;
+        _frame  = frame;
+
+        if args.motion == True:
+            try:
+                _frame  = cv2.cvtColor( _frame, cv2.COLOR_BGR2GRAY );
+                last_frame = cv2.cvtColor( last_frame, cv2.COLOR_BGR2GRAY );
+                _frame  = cv2.GaussianBlur( _frame, ( 21, 21 ), 0 );
+                last_frame  = cv2.GaussianBlur( last_frame, ( 21, 21 ), 0 );
+                diff_frame = cv2.absdiff( _frame, last_frame );
+                diff_frame = cv2.threshold( diff_frame, 30, 255, cv2.THRESH_BINARY )[1];
+                diff_frame = cv2.dilate( diff_frame, None, iterations = 2 );
+                if int( cv2.countNonZero( diff_frame ) ) / size < args.sensitivity:
+                    capture = False;
+            except:
+                capture = False;
+
+        if capture == True:
+            try:
+                print( os.path.join( path, "img_" + str( int( time.time() ) ) + "_n" + str( screen_shot ) + ".jpg" ) )
+                # cv2.imwrite( os.path.join( path, "img_" + str( int( time.time() ) ) + "_n" + str( screen_shot ) + ".jpg" ), frame );
+            except:
+                print( "[ ERRROR ] Writing File" );
         if args.delay > 3: # only print if time delay great enough
             print( "Screenshot: " + str( screen_shot ) );
         screen_shot = screen_shot + 1;
         wait = time.time() + args.delay;
+        last_frame = frame;
     if cv2.waitKey( 1 ) == 27:
         break  # esc to quit
 cv2.destroyAllWindows()
